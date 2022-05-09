3b75f2687397486f0e9f16af600ac5d2e4182c9f
Ash <ash@burnt.com>
2022-05-09 13:54:27 -0400

    scaffolded schedule module

--- app/app.go
+++ app/app.go
@@ -2,9 +2,6 @@ package app
 
 import (
 	"fmt"
-	wasmapp "github.com/CosmWasm/wasmd/app"
-	wasmclient "github.com/CosmWasm/wasmd/x/wasm/client"
-	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
 	"io"
 	"net/http"
 	"os"
@@ -96,7 +93,10 @@ import (
 	tmos "github.com/tendermint/tendermint/libs/os"
 	dbm "github.com/tendermint/tm-db"
 
+	wasmapp "github.com/CosmWasm/wasmd/app"
 	"github.com/CosmWasm/wasmd/x/wasm"
+	wasmclient "github.com/CosmWasm/wasmd/x/wasm/client"
+	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
 
 	"github.com/tendermint/starport/starport/pkg/cosmoscmd"
 	"github.com/tendermint/starport/starport/pkg/openapiconsole"
@@ -107,6 +107,9 @@ import (
 	burntmodule "github.com/BurntFinance/burnt/x/burnt"
 	burntmodulekeeper "github.com/BurntFinance/burnt/x/burnt/keeper"
 	burntmoduletypes "github.com/BurntFinance/burnt/x/burnt/types"
+	schedulemodule "github.com/BurntFinance/burnt/x/schedule"
+	schedulemodulekeeper "github.com/BurntFinance/burnt/x/schedule/keeper"
+	schedulemoduletypes "github.com/BurntFinance/burnt/x/schedule/types"
 	// this line is used by starport scaffolding # stargate/app/moduleImport
 )
 
@@ -219,6 +222,7 @@ var (
 		transfer.AppModuleBasic{},
 		vesting.AppModuleBasic{},
 		burntmodule.AppModuleBasic{},
+		schedulemodule.AppModuleBasic{},
 		wasm.AppModuleBasic{},
 		// this line is used by starport scaffolding # stargate/app/moduleBasic
 
@@ -295,6 +299,8 @@ type WasmApp struct {
 	ScopedWasmKeeper     capabilitykeeper.ScopedKeeper
 
 	BurntKeeper burntmodulekeeper.Keeper
+
+	ScheduleKeeper schedulemodulekeeper.Keeper
 	// this line is used by starport scaffolding # stargate/app/keeperDeclaration
 
 	// mm is the module manager
@@ -337,6 +343,7 @@ func NewWasmApp(
 		govtypes.StoreKey, paramstypes.StoreKey, ibchost.StoreKey, upgradetypes.StoreKey, feegrant.StoreKey,
 		evidencetypes.StoreKey, ibctransfertypes.StoreKey, capabilitytypes.StoreKey,
 		burntmoduletypes.StoreKey, authzkeeper.StoreKey, wasm.StoreKey,
+		schedulemoduletypes.StoreKey,
 		// this line is used by starport scaffolding # stargate/app/storeKey
 	)
 	tkeys := sdk.NewTransientStoreKeys(paramstypes.TStoreKey)
@@ -484,6 +491,14 @@ func NewWasmApp(
 	)
 	burntModule := burntmodule.NewAppModule(appCodec, app.BurntKeeper, app.AccountKeeper, app.BankKeeper)
 
+	app.ScheduleKeeper = *schedulemodulekeeper.NewKeeper(
+		appCodec,
+		keys[schedulemoduletypes.StoreKey],
+		keys[schedulemoduletypes.MemStoreKey],
+		app.GetSubspace(schedulemoduletypes.ModuleName),
+	)
+	scheduleModule := schedulemodule.NewAppModule(appCodec, app.ScheduleKeeper, app.AccountKeeper, app.BankKeeper)
+
 	// this line is used by starport scaffolding # stargate/app/keeperDefinition
 
 	// Create static IBC router, add transfer route, then set and seal it
@@ -523,6 +538,7 @@ func NewWasmApp(
 		params.NewAppModule(app.ParamsKeeper),
 		transferModule,
 		burntModule,
+		scheduleModule,
 		wasm.NewAppModule(appCodec, &app.WasmKeeper, app.StakingKeeper),
 		// this line is used by starport scaffolding # stargate/app/appModule
 		crisis.NewAppModule(&app.CrisisKeeper, skipGenesisInvariants), // always be last to make sure that it checks for all invariants and not only part of them
@@ -605,6 +621,7 @@ func NewWasmApp(
 		ibchost.ModuleName,
 		ibctransfertypes.ModuleName,
 		burntmoduletypes.ModuleName,
+		schedulemoduletypes.ModuleName,
 		// this line is used by starport scaffolding # stargate/app/initGenesis
 		wasm.ModuleName,
 	)
@@ -829,6 +846,7 @@ func initParamsKeeper(appCodec codec.BinaryCodec, legacyAmino *codec.LegacyAmino
 	paramsKeeper.Subspace(ibctransfertypes.ModuleName)
 	paramsKeeper.Subspace(ibchost.ModuleName)
 	paramsKeeper.Subspace(burntmoduletypes.ModuleName)
+	paramsKeeper.Subspace(schedulemoduletypes.ModuleName)
 	paramsKeeper.Subspace(wasm.ModuleName)
 	// this line is used by starport scaffolding # stargate/app/paramSubspace
 
