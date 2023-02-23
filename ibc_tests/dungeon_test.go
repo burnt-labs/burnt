14c29d4b3d080468eee185ff747b178f8ed604ba
Ash <ash@burnt.com>
2023-02-23 10:31:48 -0800

    run with one val and one fn

--- ibc_tests/dungeon_test.go
+++ ibc_tests/dungeon_test.go
@@ -41,7 +41,7 @@ func TestDungeonTransferBlock(t *testing.T) {
 	ctx := context.Background()
 
 	var numFullNodes = 1
-	var numValidators = 0
+	var numValidators = 1
 
 	// pulling image from env to foster local dev
 	imageTag := os.Getenv("BURNT_IMAGE")
